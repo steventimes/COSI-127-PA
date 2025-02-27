77e112e9facccbf3d5e55db60d209a8bc0ea2f97
steventimes <stevenyang0316@gmail.com>
2025-02-27 16:50:33 -0500

    PA 1.1 final version if no other minor problems found

--- YourOwnIMDb-main/data/database_setup.sql
+++ YourOwnIMDb-main/data/database_setup.sql
@@ -15,18 +15,18 @@ CREATE TABLE IF NOT EXISTS Genre (
     mpid INT,
     genre_name VARCHAR(255),
     PRIMARY KEY (mpid, genre_name),
-    FOREIGN KEY (mpid) REFERENCES MotionPicture(id)
+    FOREIGN KEY (mpid) REFERENCES MotionPicture(id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS Movie(
     mpid INT PRIMARY KEY,
     boxoffice_collection FLOAT,
-    FOREIGN KEY (mpid) REFERENCES MotionPicture(id),
+    FOREIGN KEY (mpid) REFERENCES MotionPicture(id) ON DELETE CASCADE,
     CONSTRAINT boxoffice_collection_check CHECK (boxoffice_collection >= 0)
 );
 CREATE TABLE IF NOT EXISTS Series(
     mpid INT PRIMARY KEY,
     season_count TINYINT,
-    FOREIGN KEY (mpid) REFERENCES MotionPicture(id),
+    FOREIGN KEY (mpid) REFERENCES MotionPicture(id) ON DELETE CASCADE,
     CONSTRAINT season_count_check CHECK (season_count >= 1)
 );
 CREATE TABLE IF NOT EXISTS Users(
@@ -38,13 +38,13 @@ CREATE TABLE IF NOT EXISTS Likes(
     uemail VARCHAR(255),
     mpid INT,
     PRIMARY KEY (uemail, mpid),
-    FOREIGN KEY (uemail) REFERENCES Users(email),
-    FOREIGN KEY (mpid) REFERENCES MotionPicture(id)
+    FOREIGN KEY (uemail) REFERENCES Users(email) ON DELETE CASCADE,
+    FOREIGN KEY (mpid) REFERENCES MotionPicture(id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS People(
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255),
-    nationality VARCHAR(255),
+    nationality VARCHAR(56),
     dob DATE,
     gender CHAR(1),
     CONSTRAINT gender_check CHECK (gender IN ('M', 'F', 'O'))
@@ -54,8 +54,9 @@ CREATE TABLE IF NOT EXISTS Role(
     pid INT,
     role_name VARCHAR(255),
     PRIMARY KEY (mpid, pid, role_name),
-    FOREIGN KEY (mpid) REFERENCES MotionPicture(id),
-    FOREIGN KEY (pid) REFERENCES People(id)
+    FOREIGN KEY (mpid) REFERENCES MotionPicture(id) ON DELETE CASCADE,
+    FOREIGN KEY (pid) REFERENCES People(id) ON DELETE
+    SET NULL
 );
 CREATE TABLE IF NOT EXISTS Award(
     mpid INT,
@@ -63,8 +64,10 @@ CREATE TABLE IF NOT EXISTS Award(
     award_name VARCHAR(255),
     award_year YEAR,
     PRIMARY KEY (mpid, pid, award_name, award_year),
-    FOREIGN KEY (mpid) REFERENCES MotionPicture(id),
-    FOREIGN KEY (pid) REFERENCES People(id)
+    FOREIGN KEY (mpid) REFERENCES MotionPicture(id) ON DELETE
+    SET NULL,
+        FOREIGN KEY (pid) REFERENCES People(id) ON DELETE
+    SET NULL
 );
 CREATE TABLE IF NOT EXISTS Location(
     mpid INT,
