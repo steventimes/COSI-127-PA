100db4803e5d8498eaa589b64f73a3289b9da8e1
steventimes <stevenyang0316@gmail.com>
2025-03-14 14:44:56 -0400

    update to pa1.2

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv("ini.env")  # Load environment variables from .env file
-
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_DATABASE = os.getenv("DB_DATABASE")
